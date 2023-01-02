9de7a2e2f6fbf63dc3d164569b92f49988007e2b
Ben Chong <benchong1999@yahoo.com.au>
2023-01-02 16:27:58 +1000

    Remove unused btn classname

--- clipboard-landing-page/src/components/Button/Button.tsx
+++ clipboard-landing-page/src/components/Button/Button.tsx
@@ -1,10 +1,13 @@
 import { ButtonProps } from "./ButtonProps";
+import styles from "./Button.module.css";
 
 const Button = ({ children, variant = "primary" }: ButtonProps) => {
   const variantClassName =
     variant === "primary" ? "btn-primary" : "btn-secondary";
 
-  return <button className={`btn ${variantClassName}`}>{children}</button>;
+  return (
+    <button className={`${styles.btn} ${variantClassName}`}>{children}</button>
+  );
 };
 
 export default Button;
