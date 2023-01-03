d15675427001ea519d910b9016e6a0370e842f57
Ben Chong <benchong1999@yahoo.com.au>
2023-01-03 14:06:01 +1000

    Extract IconButton display styles

--- clipboard-landing-page/src/components/IconButton/IconButton.tsx
+++ clipboard-landing-page/src/components/IconButton/IconButton.tsx
@@ -1,11 +1,12 @@
 import Icon from "../Icon";
+import styles from "./IconButton.module.css";
 import { IconButtonProps } from "./IconButtonProps";
 
 const IconButton = ({ name, href, size = 24 }: IconButtonProps) => (
   <a
     href={href}
     target="_blank"
-    className="inline-block transition hover:text-strongCyan-500 focus:text-strongCyan-500"
+    className={`${styles.iconButton} transition hover:text-strongCyan-500 focus:text-strongCyan-500`}
   >
     <Icon name={name} size={size} />
   </a>
