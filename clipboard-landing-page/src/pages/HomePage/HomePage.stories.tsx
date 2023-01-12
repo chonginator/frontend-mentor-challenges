4c45e5b6bae1ad5fccc2279d769fa49beb270a30
Ben Chong <benchong1999@yahoo.com.au>
2023-01-12 15:35:12 +1000

    Add HomePage component

---
+++ clipboard-landing-page/src/pages/HomePage/HomePage.stories.tsx
@@ -0,0 +1,12 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { HomePage } from "./HomePage";
+
+export default {
+  title: "Pages/HomePage",
+  component: HomePage,
+} as ComponentMeta<typeof HomePage>
+
+const Template: ComponentStory<typeof HomePage> = () => <HomePage />
+
+export const Default = Template.bind({})
+
---
+++ clipboard-landing-page/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <main>
+      <h1>Hello there!</h1>
+    </main>
+  )
+}
+
+export { HomePage };
