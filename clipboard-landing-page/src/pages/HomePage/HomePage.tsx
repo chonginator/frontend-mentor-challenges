66270a3b8e9630bb4948145ad01bce56ebcf3847
Ben Chong <benchong1999@yahoo.com.au>
2023-01-12 15:39:07 +1000

    Make height of home page 100%

--- clipboard-landing-page/src/pages/HomePage/HomePage.tsx
+++ clipboard-landing-page/src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,8 @@
+import styles from "./HomePage.module.css";
+
 const HomePage = () => {
   return (
-    <main>
+    <main className={styles.container}>
       <h1>Hello there!</h1>
     </main>
   )
