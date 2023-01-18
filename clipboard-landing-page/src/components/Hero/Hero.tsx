3f9cd08a627c10a97e99c269fc21bc5c2eafabe8
Ben Chong <benchong1999@yahoo.com.au>
2023-01-18 17:10:02 +0800

    Style hero section

--- clipboard-landing-page/src/components/Hero/Hero.tsx
+++ clipboard-landing-page/src/components/Hero/Hero.tsx
@@ -1,16 +1,19 @@
 import Logo from "../Logo";
 import Button from "../Button";
+import styles from "./Hero.module.css";
 
 const Hero = () => {
   return (
-    <section>
+    <section className={styles.container}>
       <Logo />
-      <h1>A history of everything you copy</h1>
-      <p>
-        Clipboard allows you to track and organize everything you copy.
-        Instantly access your clipboard on all your devices.
-      </p>
-      <div>
+      <div className={styles.titleContainer}>
+        <h1 className={styles.title}>A history of everything you copy</h1>
+        <p className={styles.subtitle}>
+          Clipboard allows you to track and organize everything you copy.
+          Instantly access your clipboard on all your devices.
+        </p>
+      </div>
+      <div className={styles.buttonContainer}>
         <Button>Download for iOS</Button>
         <Button variant="secondary">Download for Mac</Button>
       </div>
