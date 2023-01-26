28630192890cb88f1cb4b2a72f849232b981d424
Ben Chong <benchong1999@yahoo.com.au>
2023-01-26 16:23:38 +0800

    Remove Figma file link from home page stories

--- clipboard-landing-page/src/pages/HomePage/HomePage.stories.tsx
+++ clipboard-landing-page/src/pages/HomePage/HomePage.stories.tsx
@@ -5,10 +5,6 @@ export default {
   title: "Pages/HomePage",
   component: HomePage,
   parameters: {
-    design: {
-      type: "figspec",
-      url: "https://www.figma.com/file/JULngfX5WXYZmDmIhM9jk3/clipboard-landing-page?t=JzBfWhFg4ZGSGn63-6",
-    },
     layout: "fullscreen",
   },
 } as ComponentMeta<typeof HomePage>;
