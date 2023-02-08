cbbf4d97dffbea7c275354267d5d04f5a8cd3134
Ben Chong <benchong1999@yahoo.com.au>
2023-02-08 15:50:30 +1000

    Add colours, font and font sizes to theme

--- clipboard-landing-page-v2/tailwind.config.js
+++ clipboard-landing-page-v2/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      strongCyan: "hsl(171, 66%, 44%)",
+      lightBlue: "hsl(233, 100%, 69%)",
+      darkGrayishBlue: "hsl(210, 10%, 33%)",
+      grayishBlue: "hsl(201, 11%, 66%)",
+    },
+    fontFamily: {
+      sans: ["Bai Jamjuree", "sans-serif"],
+    },
+    fontSize: {
+      "3xl": "1.75rem",
+      "5xl": "2.875rem",
+    },
   },
   plugins: [],
 };
