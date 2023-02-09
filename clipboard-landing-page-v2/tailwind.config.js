58ee317a28d5c5e8ef5da58abaf81134f2e7de27
Ben Chong <benchong1999@yahoo.com.au>
2023-02-09 13:21:31 +1000

    Add custom font size utilities

--- clipboard-landing-page-v2/tailwind.config.js
+++ clipboard-landing-page-v2/tailwind.config.js
@@ -3,17 +3,20 @@ module.exports = {
   content: ["./src/**/*.html"],
   theme: {
     extend: {
-      strongCyan: "hsl(171, 66%, 44%)",
-      lightBlue: "hsl(233, 100%, 69%)",
-      darkGrayishBlue: "hsl(210, 10%, 33%)",
-      grayishBlue: "hsl(201, 11%, 66%)",
+      colors: {
+        strongCyan: "hsl(171, 66%, 44%)",
+        lightBlue: "hsl(233, 100%, 69%)",
+        darkGrayishBlue: "hsl(210, 10%, 33%)",
+        grayishBlue: "hsl(201, 11%, 66%)",
+      },
+      fontSize: {
+        28: "1.75rem",
+        32: "2rem",
+        46: "2.875rem",
+      },
     },
     fontFamily: {
       sans: ["Bai Jamjuree", "sans-serif"],
-    },
-    fontSize: {
-      "3xl": "1.75rem",
-      "5xl": "2.875rem",
     },
   },
   plugins: [],
