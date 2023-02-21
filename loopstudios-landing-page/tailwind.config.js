483b5e0433934cafb4d735217346231c886e45f8
Ben Chong <benchong1999@yahoo.com.au>
2023-02-21 13:42:59 +1000

    Add higher res background images to custom Tailwind theme

--- loopstudios-landing-page/tailwind.config.js
+++ loopstudios-landing-page/tailwind.config.js
@@ -2,17 +2,22 @@
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
-    fontSize: {
-      base: "15px",
-    },
     extend: {
-      fontFamily: {
-        body: ["Alata", "sans-serif"],
-        heading: ["Josefin Sans", "sans-serif"],
+      backgroundImage: {
+        "hero-image-mobile": "url('/src/assets/images/mobile/image-hero.jpg')",
+        "hero-image-desktop":
+          "url('/src/assets/images/desktop/image-hero.jpg')",
       },
       colors: {
         darkGray: "hsl(0, 0%, 55%)",
         veryDarkGray: "hsl(0, 0%, 41%)",
+      },
+      fontFamily: {
+        body: ["Alata", "sans-serif"],
+        heading: ["Josefin Sans", "sans-serif"],
+      },
+      fontSize: {
+        base: "15px",
       },
     },
   },
