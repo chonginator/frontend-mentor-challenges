3087e54c2325eba87bbfa678690806ae610d753d
Ben Chong <benchong1999@yahoo.com.au>
2023-02-23 14:59:21 +1000

    Style work gallery section

--- loopstudios-landing-page/tailwind.config.js
+++ loopstudios-landing-page/tailwind.config.js
@@ -4,9 +4,44 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "hero-image-mobile": "url('/src/assets/images/mobile/image-hero.jpg')",
-        "hero-image-desktop":
-          "url('/src/assets/images/desktop/image-hero.jpg')",
+        "hero-mobile": "url('/src/assets/images/mobile/image-hero.jpg')",
+        "hero-desktop": "url('/src/assets/images/desktop/image-hero.jpg')",
+        "project-gradient-overlay-mobile":
+          "linear-gradient(to right, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.0001))",
+        "project-gradient-overlay-desktop":
+          "linear-gradient(to top, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0))",
+        "project-deep-earth-mobile":
+          "url('/src/assets/images/mobile/image-deep-earth.jpg')",
+        "project-deep-earth-desktop":
+          "url('/src/assets/images/desktop/image-deep-earth.jpg')",
+        "project-night-arcade-mobile":
+          "url('/src/assets/images/mobile/image-night-arcade.jpg')",
+        "project-night-arcade-desktop":
+          "url('/src/assets/images/desktop/image-night-arcade.jpg')",
+        "project-soccer-team-mobile":
+          "url('/src/assets/images/mobile/image-soccer-team.jpg')",
+        "project-soccer-team-desktop":
+          "url('/src/assets/images/desktop/image-soccer-team.jpg')",
+        "project-grid-mobile":
+          "url('/src/assets/images/mobile/image-grid.jpg')",
+        "project-grid-desktop":
+          "url('/src/assets/images/desktop/image-grid.jpg')",
+        "project-from-above-mobile":
+          "url('/src/assets/images/mobile/image-from-above.jpg')",
+        "project-from-above-desktop":
+          "url('/src/assets/images/desktop/image-from-above.jpg')",
+        "project-pocket-borealis-mobile":
+          "url('/src/assets/images/mobile/image-pocket-borealis.jpg')",
+        "project-pocket-borealis-desktop":
+          "url('/src/assets/images/desktop/image-pocket-borealis.jpg')",
+        "project-curiosity-mobile":
+          "url('/src/assets/images/mobile/image-curiosity.jpg')",
+        "project-curiosity-desktop":
+          "url('/src/assets/images/desktop/image-curiosity.jpg')",
+        "project-fisheye-mobile":
+          "url('/src/assets/images/mobile/image-fisheye.jpg')",
+        "project-fisheye-desktop":
+          "url('/src/assets/images/desktop/image-fisheye.jpg')",
       },
       colors: {
         darkGray: "hsl(0, 0%, 55%)",
@@ -17,7 +52,9 @@ module.exports = {
         heading: ["Josefin Sans", "sans-serif"],
       },
       fontSize: {
-        base: ["15px", "25px"],
+        sm: ["0.875rem", "0.875rem"],
+        base: ["0.9375rem", "1.5rem"],
+        "2xl": ["1.5rem", "1.5rem"],
         "3xl": ["2rem", "2rem"],
       },
     },
