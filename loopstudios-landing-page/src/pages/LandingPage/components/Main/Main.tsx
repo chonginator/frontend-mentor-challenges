77f4172d7157aa4eae487e10829a7e4b5f7f7a93
Ben Chong <benchong1999@yahoo.com.au>
2023-03-01 13:56:15 +1000

    Increase max-width of about section text

--- loopstudios-landing-page/src/pages/LandingPage/components/Main/Main.tsx
+++ loopstudios-landing-page/src/pages/LandingPage/components/Main/Main.tsx
@@ -17,7 +17,7 @@ function Main() {
         <div className="mx-auto max-w-[1111px]">
           <div className="flex flex-col lg:flex-row lg:items-end">
             <img src={interactiveVRImage} alt="Man using a VR headset" />
-            <div className="mx-auto flex max-w-[17rem] flex-col gap-4 bg-white text-center sm:max-w-none lg:absolute lg:right-[164px] lg:-bottom-[1px] lg:top-[37%] lg:max-w-[541px] lg:pt-[6.67%] lg:pl-[6.67%] lg:text-left">
+            <div className="mx-auto flex max-w-[17rem] flex-col gap-4 bg-white text-center sm:max-w-none lg:absolute lg:right-[164px] lg:-bottom-[1px] lg:top-[37%] lg:max-w-[34.375rem] lg:pt-[6.67%] lg:pl-[6.67%] lg:text-left">
               <h2 className="mt-12 font-heading text-3xl font-light uppercase lg:mt-0 lg:text-5xl">
                 The leader in interactive VR
               </h2>
