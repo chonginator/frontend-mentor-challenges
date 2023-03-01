c1802de882c141e53cabe2576537448723f0c0b1
Ben Chong <benchong1999@yahoo.com.au>
2023-03-01 15:57:48 +1000

    Add default Header story

---
+++ loopstudios-landing-page/src/components/Header/Header.stories.tsx
@@ -0,0 +1,11 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import Header from "./Header";
+
+export default {
+  title: "Components/Header",
+  component: Header,
+} as ComponentMeta<typeof Header>;
+
+const Template: ComponentStory<typeof Header> = () => <Header />;
+
+export const Default = Template.bind({});
