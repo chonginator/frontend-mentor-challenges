1a99ab79bf7b101d480c65040d8e92f555c9ba1e
Ben Chong <benchong1999@yahoo.com.au>
2023-03-03 15:22:28 +1000

    Move HeaderWrapper component back into Header file

--- loopstudios-landing-page/src/components/Header/Header.tsx
+++ loopstudios-landing-page/src/components/Header/Header.tsx
@@ -1,11 +1,19 @@
 import Nav from "./Nav";
-import HeaderWrapper from "./HeaderWrapper";
+import MaxWidthWrapper from "../MaxWidthWrapper";
 
 function Header() {
   return (
     <HeaderWrapper>
       <Nav />
     </HeaderWrapper>
+  );
+}
+
+function HeaderWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <header className="absolute isolate z-10 mt-16 w-full px-6 font-body text-white">
+      <MaxWidthWrapper>{children}</MaxWidthWrapper>
+    </header>
   );
 }
 
