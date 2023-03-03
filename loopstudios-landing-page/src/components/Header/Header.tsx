2ddf36df3b388959db1b8a46b0fa420fc844f80d
Ben Chong <benchong1999@yahoo.com.au>
2023-03-03 15:43:28 +1000

    Rename Header wrapper component

--- loopstudios-landing-page/src/components/Header/Header.tsx
+++ loopstudios-landing-page/src/components/Header/Header.tsx
@@ -3,13 +3,13 @@ import MaxWidthWrapper from "../MaxWidthWrapper";
 
 function Header() {
   return (
-    <HeaderWrapper>
+    <Wrapper>
       <Nav />
-    </HeaderWrapper>
+    </Wrapper>
   );
 }
 
-function HeaderWrapper({ children }: { children: React.ReactNode }) {
+function Wrapper({ children }: { children: React.ReactNode }) {
   return (
     <header className="absolute isolate z-10 mt-16 w-full px-6 font-body text-white">
       <MaxWidthWrapper>{children}</MaxWidthWrapper>
