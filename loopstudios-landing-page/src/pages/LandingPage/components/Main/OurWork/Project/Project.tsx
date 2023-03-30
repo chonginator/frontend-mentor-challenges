39c87b5a5d5f0f41739ef4bf9a26949e3e941973
Ben Chong <benchong1999@yahoo.com.au>
2023-03-31 09:05:24 +1000

    Remove mix-blend-overlay class

--- loopstudios-landing-page/src/pages/LandingPage/components/Main/OurWork/Project/Project.tsx
+++ loopstudios-landing-page/src/pages/LandingPage/components/Main/OurWork/Project/Project.tsx
@@ -13,7 +13,7 @@ function Project({ name, title }: ProjectProps) {
 
   return (
     <div
-      className={`${mobileBgClass} ${desktopBgClass} group relative flex min-h-[120px] cursor-pointer items-end bg-cover bg-no-repeat px-5 py-4 bg-blend-overlay mix-blend-overlay transition-colors before:absolute before:inset-0 before:bg-project-gradient-overlay-mobile before:bg-cover before:bg-no-repeat hover:bg-white/75 hover:before:bg-none md:min-h-[450px] lg:px-9 lg:py-7 lg:before:bg-project-gradient-overlay-desktop`}
+      className={`${mobileBgClass} ${desktopBgClass} group relative flex min-h-[120px] cursor-pointer items-end bg-cover bg-no-repeat px-5 py-4 bg-blend-overlay transition-colors before:absolute before:inset-0 before:bg-project-gradient-overlay-mobile before:bg-cover before:bg-no-repeat hover:bg-white/75 hover:before:bg-none md:min-h-[450px] lg:px-9 lg:py-7 lg:before:bg-project-gradient-overlay-desktop`}
     >
       <h3 className="isolate text-left font-heading text-2xl font-light uppercase text-white group-hover:text-black lg:text-3xl">
         {titleWithLineBreaks}
