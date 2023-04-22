3a7c549c4ca78b0d10979eaf7dd015fe34a007a7
Ben Chong <benchong1999@yahoo.com.au>
2023-04-22 16:56:17 +1000

    Style FeaturedStory

--- news-homepage/src/components/FeaturedStory/FeaturedStory.tsx
+++ news-homepage/src/components/FeaturedStory/FeaturedStory.tsx
@@ -1,17 +1,27 @@
+import { FEATURED_STORY } from "../../data";
+
 /* eslint-disable jsx-a11y/anchor-is-valid */
 function FeaturedStory() {
+  const { headline, subhead, image } = FEATURED_STORY;
   return (
-    <div>
-      <img src="/images/image-web-3-desktop.jpg" alt="Web 3" />
-      <div>
-        <h1>The Bright Future of Web 3.0?</h1>
-        <div>
-          <p>
-            We dive into the next evolution of the web that claims to put the
-            power of the platforms back into the hands of the people. But is it
-            really fulfilling its promise?
-          </p>
-          <a href="#">Read more</a>
+    <div className="col-span-2">
+      <img
+        className="h-[300px] w-full object-cover object-top md:w-[730px]"
+        src={image.src}
+        alt={image.alt}
+      />
+      <div className="mt-6 grid gap-4 md:mt-7 md:grid-cols-2 md:gap-[30px]">
+        <h1 className="text-lg font-extrabold text-black md:text-xl">
+          {headline}
+        </h1>
+        <div className="flex flex-col items-start justify-between gap-6">
+          <p className="text-darkGray">{subhead}</p>
+          <a
+            className="bg-orange px-9 py-3 text-[14px] font-bold uppercase tracking-[4.38px] text-white transition-colors hover:bg-black"
+            href="#"
+          >
+            Read more
+          </a>
         </div>
       </div>
     </div>
