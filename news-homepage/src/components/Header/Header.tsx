bdd87c1db15375d7e22e8965593148f95a7b8fc0
Ben Chong <benchong1999@yahoo.com.au>
2023-04-22 21:26:14 +1000

    Extract MainMenu component

--- news-homepage/src/components/Header/Header.tsx
+++ news-homepage/src/components/Header/Header.tsx
@@ -6,52 +6,58 @@ function Header() {
   return (
     <header className="flex items-center justify-between">
       <Logo />
-      <nav className="hidden md:block" aria-label="Main menu">
-        <ul className="flex gap-10 text-darkGray">
-          <li>
-            <a
-              href="#"
-              className="transition hover:text-orange focus:text-orange"
-            >
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="transition hover:text-orange focus:text-orange"
-            >
-              New
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="transition hover:text-orange focus:text-orange"
-            >
-              Popular
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="transition hover:text-orange focus:text-orange"
-            >
-              Trending
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="transition hover:text-orange focus:text-orange"
-            >
-              Categories
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <MainMenu />
       <HamburgerMenu />
     </header>
+  );
+}
+
+function MainMenu() {
+  return (
+    <nav className="hidden md:block" aria-label="Main menu">
+      <ul className="flex gap-10 text-darkGray">
+        <li>
+          <a
+            href="#"
+            className="transition hover:text-orange focus:text-orange"
+          >
+            Home
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="transition hover:text-orange focus:text-orange"
+          >
+            New
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="transition hover:text-orange focus:text-orange"
+          >
+            Popular
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="transition hover:text-orange focus:text-orange"
+          >
+            Trending
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="transition hover:text-orange focus:text-orange"
+          >
+            Categories
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
