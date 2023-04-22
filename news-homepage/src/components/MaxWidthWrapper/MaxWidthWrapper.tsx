d5b1ab40df46cdaae72234d49c38cf180af48ac3
Ben Chong <benchong1999@yahoo.com.au>
2023-04-22 15:14:40 +1000

    Remove padding on MaxWidthWrapper for desktop

--- news-homepage/src/components/MaxWidthWrapper/MaxWidthWrapper.tsx
+++ news-homepage/src/components/MaxWidthWrapper/MaxWidthWrapper.tsx
@@ -3,7 +3,7 @@ import { MaxWidthWrapperProps } from "./MaxWidthWrapperProps";
 
 function MaxWidthWrapper({ children, className }: MaxWidthWrapperProps) {
   return (
-    <div className={clsx("mx-auto max-w-[1110px] px-4", className)}>
+    <div className={clsx("mx-auto max-w-[1110px] px-4 md:px-0", className)}>
       {children}
     </div>
   );
