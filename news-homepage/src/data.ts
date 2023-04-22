637ef4a3b19e436fc2eb22879c1da493a9268dc5
Ben Chong <benchong1999@yahoo.com.au>
2023-04-22 16:24:23 +1000

    Add data for news stories

---
+++ news-homepage/src/data.ts
@@ -0,0 +1,53 @@
+export const FEATURED_STORY = {
+  headline: "The Bright Future of Web 3.0?",
+  subhead:
+    "We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?",
+  image: {
+    src: "/images/image-web-3.jpg",
+    alt: "Blue, orange, and red blocks of various shapes and sizes",
+  },
+};
+
+export const RECENT_STORIES = [
+  {
+    headline: "Hydrogen VS Electric Cars",
+    subhead: "Will hydrogen-fueled cars ever catch up to EVs?",
+  },
+  {
+    headline: "The Downsides of AI Artistry",
+    subhead:
+      "What are the possible adverse effects of on-demand AI image generation?",
+  },
+  {
+    headline: "Is VC Funding Drying Up?",
+    subhead:
+      "Private funding by VC firms is down 50% YOY. We take a look at what that means.",
+  },
+];
+
+export const TOP_STORIES = [
+  {
+    headline: "Reviving Retro PCs",
+    subhead: "What happens when old PCs are given modern upgrades?",
+    image: {
+      src: "/images/image-retro-pcs.jpg",
+      alt: "Retro PC",
+    },
+  },
+  {
+    headline: "Top 10 Laptops of 2022",
+    subhead: "Our best picks for various needs and budgets.",
+    image: {
+      src: "/images/image-top-laptops.jpg",
+      alt: "Computer Keyboard",
+    },
+  },
+  {
+    headline: "The Growth of Gaming",
+    subhead: "How the pandemic has sparked fresh opportunities.",
+    image: {
+      src: "/images/image-gaming-growth.jpg",
+      alt: "PS5 controller",
+    },
+  },
+];
