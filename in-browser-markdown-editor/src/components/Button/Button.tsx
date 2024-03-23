339bdb949f51abde8757cd1aa98507aafa840a22
Ben Chong <benchong1999@yahoo.com.au>
2024-03-23 10:47:32 +1000

    Add default styles to Button

--- in-browser-markdown-editor/src/components/Button/Button.tsx
+++ in-browser-markdown-editor/src/components/Button/Button.tsx
@@ -7,10 +7,10 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 function Button({ icon: Icon, children, ...delegated }: ButtonProps) {
   return (
     <button
-      className="font-sans text-[15px] font-normal text-gray-100 bg-orange-600 hover:bg-orange-400 rounded-[4px] py-[10px] px-4 transition-colors"
+      className="flex flex-row gap-2 items-center font-sans text-[15px] font-normal text-gray-100 bg-orange-600 hover:bg-orange-400 rounded-[4px] py-[10px] px-4 transition-colors"
       {...delegated}
     >
-      <span>{Icon && <Icon />}</span>
+      <span>{Icon && <Icon className="size-4 fill-current stroke-2" />}</span>
       <span>{children}</span>
     </button>
   );
